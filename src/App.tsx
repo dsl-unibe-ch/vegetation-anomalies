60b794dc9a4bb41efb32aefc5276cf1addb8418c
vkovtun <viktor.kovtun@id.ethz.ch>
2024-10-14 14:35:31 +0200

    Removed commented code.

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,10 @@ import React from 'react';
 import './App.css';
 import MapWithRaster from "./MapWithRaster";
 
-function App() {
-  return (
+const App = () => (
     <div className="App">
-      {/*<header className="App-header">*/}
-      {/*  <img src={logo} className="App-logo" alt="logo" />*/}
-      {/*  <p>*/}
-      {/*    Edit <code>src/App.tsx</code> and save to reload.*/}
-      {/*  </p>*/}
-      {/*  <a*/}
-      {/*    className="App-link"*/}
-      {/*    href="https://reactjs.org"*/}
-      {/*    target="_blank"*/}
-      {/*    rel="noopener noreferrer"*/}
-      {/*  >*/}
-      {/*    Learn React*/}
-      {/*  </a>*/}
-      {/*</header>*/}
-
-      <MapWithRaster/>
+        <MapWithRaster/>
     </div>
-  );
-}
+);
 
 export default App;
