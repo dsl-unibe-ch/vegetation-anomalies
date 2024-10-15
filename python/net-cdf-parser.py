6e16f654f501b0e43adde5fdca41c5986cee5ffe
vkovtun <viktor.kovtun@id.ethz.ch>
2024-10-15 11:21:25 +0200

    Working image output.

--- python/net-cdf-parser.py
+++ python/net-cdf-parser.py
@@ -1,49 +1,57 @@
-import base64
 import json
 import os
-from io import BytesIO
 
 import xarray as xr
 from matplotlib import pyplot as plt
 from tqdm import tqdm
 
 cubes_dir = "../data/cubes_demo"
-cube_files = [f for f in os.listdir(cubes_dir) if f.startswith('anomalies') and f.endswith('.nc')]
-
-cube_predictions = []
+cubes_output_dir = "../data/cubes_demo_output"
+cube_files = [f for f in os.listdir(cubes_dir) if f.startswith("anomalies") and f.endswith(".nc")]
 
 for file in cube_files:
+    bare_file_name = os.path.splitext(os.path.basename(file))[0]
     dataset = xr.open_dataset(os.path.join(cubes_dir, file))
 
+    output_directory_path = os.path.join(cubes_output_dir, bare_file_name)
+    os.makedirs(output_directory_path, exist_ok=True)
+
     # Extract latitude, longitude, anomaly, and time values
-    lat = dataset['lat'].values.tolist()
-    lon = dataset['lon'].values.tolist()
-    time = dataset['time'].values.astype(str).tolist()
+    lat = dataset["lat"].values.tolist()
+    lon = dataset["lon"].values.tolist()
+    time = dataset["time"].values.astype(str).tolist()
 
-    # print(f"{time}")
+    anomaly_data = dataset["anomaly"]
 
-    anomaly_data = dataset['anomaly']
+    # Create a dictionary to store lat, lon, and images
+    cube_metadata = {
+        "lat": lat,
+        "lon": lon,
+        "images": []
+    }
 
     # Iterate over the time dimension to extract anomaly images at each time step
     for i, time_step in enumerate(tqdm(time, desc=f"Processing {file}", unit="time_step")):
         data_values = anomaly_data.isel(time=i).values
-        plt.imshow(data_values, cmap='viridis')
-        plt.axis('off')
+        plt.imshow(data_values, cmap="viridis")
+        plt.axis("off")
 
-        # Save image to buffer and encode as base64
-        buffer = BytesIO()
-        plt.savefig(buffer, format='png', bbox_inches='tight', pad_inches=0)
-        buffer.seek(0)
-        img_data = base64.b64encode(buffer.read()).decode('utf-8')
-        buffer.close()
+        fig_file_name = f"{bare_file_name}_{time_step}.png"
+        plt.savefig(os.path.join(output_directory_path, fig_file_name), format="png", bbox_inches="tight", pad_inches=0)
 
-        cube_predictions.append({
-            "lat": lat[i],
-            "lon": lon[i],
+        # # Save image to buffer and encode as base64
+        # buffer = BytesIO()
+        # plt.savefig(buffer, format="png", bbox_inches="tight", pad_inches=0)
+        # buffer.seek(0)
+        # img_data = base64.b64encode(buffer.read()).decode("utf-8")
+        # buffer.close()
+
+        cube_metadata["images"].append({
             "time": time_step,
-            "image_data": img_data
+            "image": fig_file_name
         })
 
-# Save the extracted data to a JSON file
-with open("../data/cubes_anomalies.json", "w") as f:
-    json.dump(cube_predictions, f)
+
+    # Save the extracted data to a JSON file
+    with open(os.path.join(output_directory_path, f"{bare_file_name}.json"), "w") as f:
+        json.dump(cube_metadata, f)
