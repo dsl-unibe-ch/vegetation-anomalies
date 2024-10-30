8ea7fb802f5f168e07bea2aab1d3d3c2b14ab167
vkovtun <viktor.kovtun@id.ethz.ch>
2024-10-30 17:19:36 +0100

    Serving transparent image by file-server.py for the tiles which are missing in the file system.

--- python/file-server.py
+++ python/file-server.py
@@ -1,6 +1,10 @@
 import http.server
 import os
 from http.server import SimpleHTTPRequestHandler
+from io import BytesIO
+
+from PIL import Image
+
 
 class CORSRequestHandler(SimpleHTTPRequestHandler):
     def end_headers(self):
@@ -9,9 +13,26 @@ class CORSRequestHandler(SimpleHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Headers', 'Content-Type')
         super().end_headers()
 
+    def do_GET(self):
+        # Check if the file exists
+        if not os.path.exists(self.translate_path(self.path)):
+            # File not found, send a blank 256x256 PNG image
+            self.send_response(200)
+            self.send_header('Content-type', 'image/png')
+            self.end_headers()
+
+            # Create a blank PNG image
+            blank_image = Image.new('RGBA', (128, 128), (0, 0, 0, 0))
+            img_bytes = BytesIO()
+            blank_image.save(img_bytes, format='PNG')
+            self.wfile.write(img_bytes.getvalue())
+        else:
+            # If the file exists, serve it as usual
+            super().do_GET()
+
 if __name__ == '__main__':
     # Set the working directory to where your tiles are located
-    tiles_directory = '/home/viktor/Work/Projects/UniBe/GIUB/vegetation-anomalies/vegetation-anomalies/data/cubes_demo_output'
+    tiles_directory = '../data/cubes_demo_output'
     os.chdir(tiles_directory)
 
     # Start the server
