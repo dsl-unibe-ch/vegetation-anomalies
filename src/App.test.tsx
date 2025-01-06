dcf1331162fc05642f52fd6ebbaf011c58599516
Viktor Kovtun <viktor.kovtun@unibe.ch>
2025-01-06 15:41:02 +0100

    Removed unused files.

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render} from '@testing-library/react';
-import MapWithRaster from './MapWithRaster';
-
-test('renders learn react link', () => {
-  render(<MapWithRaster />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
-});
